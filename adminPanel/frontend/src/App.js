db40378404c070a1520b64fd0a36e46a12f46064
MDSARFRAAZNASIR <mdsarfraazpctebtech20@gmail.com>
2025-11-26 10:30:55 +0530

    Push Admin Panel

---
+++ adminPanel/frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PostJobPage from './components/PostJobPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Admin Panel
+      </h1>
+      <PostJobPage/>
+    </div>
+  );
+}
+
+export default App;
